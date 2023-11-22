85010f24560b1e545f4326ed0622c8624c980303
goncamateus <mateus.gm@gmail.com>
2023-11-22 15:45:24 -0300

    wip(trajectory env)

--- envs/trajectory.py
+++ envs/trajectory.py
@@ -32,3 +32,54 @@ class TrajectoryEnv(SSLBaseEnv):
         super().__init__(
             field_type, n_robots_blue, n_robots_yellow, time_step, render_mode
         )
+
+        self._trajectory = []
+        self._trajectory_index = 0
+        self._trajectory_size = 40
+        self.reward_info = {
+            "reward_dist": 0,
+            "reward_action_var": 0,
+            "reward_continuity": 0,
+            "reward_objective": 0,
+            "reward_total": 0,
+        }
+
+    def _calculate_reward_dist(self, trajectory: np.ndarray, target: np.ndarray):
+        distances = np.linalg.norm(trajectory - target, axis=1)
+        my_arr = np.zeros(len(distances - 1))
+        for i in range(len(distances) - 1):
+            transition = distances[i] - distances[i + 1]
+            if transition < -0.05:
+                if i == 0:
+                    my_arr[i] = 1
+                else:
+                    my_arr[i] = my_arr[i - 1] + 1
+            if transition >= 0.05:
+                if i == 0:
+                    my_arr[i] = -1
+                else:
+                    if my_arr[i - 1] >= 0:
+                        my_arr[i] = -1
+                    else:
+                        my_arr[i] = my_arr[i - 1] - 1
+
+            if transition >= -0.05 and transition < 0.05:
+                if i == 0:
+                    my_arr[i] = 0
+                else:
+                    if my_arr[i - 1] <= 0:
+                        my_arr[i] = my_arr[i - 1]
+                    else:
+                        my_arr[i] = 0
+        return np.sum(my_arr) / np.arange(self._trajectory_size - 1).sum()
+
+    def _calculate_reward_continuity(self, trajectory: np.ndarray):
+        vectors = trajectory[1:] - trajectory[:-1]
+        normed_vectors = vectors / np.linalg.norm(vectors, axis=1)[:, None]
+        pairwise_dot = np.einsum("ij,ij->i", normed_vectors[:-1], normed_vectors[1:])
+        dot_in_range = (pairwise_dot > 0).astype(int)
+        return np.sum(dot_in_range) / np.arange(self._trajectory_size - 1).sum()
+
+    def _calculate_reward_action_var(self, trajectory: np.ndarray):
+        distances = np.linalg.norm(trajectory[1:] - trajectory[:-1], axis=1)
+        return np.sum(distances)
