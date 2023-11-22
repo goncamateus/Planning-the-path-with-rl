9fe308f501b708848c0dddb6ee3575876f4d2ea0
goncamateus <mgm4@cin.ufpe.br>
2023-11-22 10:13:36 -0300

    Name changes

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,28 +1,28 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="PathPlanning-v0",
+    id="Baseline-v0",
     entry_point="envs.baseline:SSLPathPlanningBaseLineEnv",
     kwargs={"field_type": 2, "n_robots_yellow": 0},
     max_episode_steps=1200,
 )
 
 register(
-    id="PathPlanning-v1",
+    id="Enhanced-v0",
     entry_point="envs.enhanced:SSLPathPlanningEnv",
     kwargs={"field_type": 2, "n_robots_yellow": 0, "repeat_action": 1},
     max_episode_steps=1200,
 )
 
 register(
-    id="PathPlanning-v2",
+    id="Enhanced-v1",
     entry_point="envs.enhanced:SSLPathPlanningEnv",
     kwargs={"field_type": 2, "n_robots_yellow": 0, "repeat_action": 16},
     max_episode_steps=75,
 )
 
 register(
-    id="PathPlanningMedium-v0",
+    id="Obstacle-v0",
     entry_point="envs.enhanced:SSLPathPlanningMediumEnv",
     kwargs={"n_robots_yellow": 1},
     max_episode_steps=75,
