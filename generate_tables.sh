ad719dfcc8b6d212531f5f86eb49de35b846b3b8
goncamateus <mgm4@cin.ufpe.br>
2023-12-16 15:05:09 -0300

    Adds obstacles-v0

--- generate_tables.sh
+++ generate_tables.sh
@@ -8,5 +8,7 @@ sbatch -w cluster-node3 -o test_4.log run_test.sh c1 Enhanced-v0
 sbatch -w cluster-node3 -o test_5.log run_test.sh c1 Enhanced-v0 --caps
 sbatch -w cluster-node4 -o test_6.log run_test.sh c1 Enhanced-v1
 sbatch -w cluster-node4 -o test_7.log run_test.sh c1 Enhanced-v1 --caps
-sbatch -w cluster-node5 -o test_8.log run_test.sh c1 Obstacle-v1
-sbatch -w cluster-node5 -o test_9.log run_test.sh c1 Obstacle-v1 --caps
+sbatch -w cluster-node4 -o test_8.log run_test.sh c1 Obstacle-v0
+sbatch -w cluster-node4 -o test_9.log run_test.sh c1 Obstacle-v0 --caps
+sbatch -w cluster-node5 -o test_10.log run_test.sh c1 Obstacle-v1
+sbatch -w cluster-node5 -o test_11.log run_test.sh c1 Obstacle-v1 --caps
